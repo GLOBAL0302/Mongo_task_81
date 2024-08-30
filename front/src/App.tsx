6c1d3714eaa6d4a791bffedf52ad88c76038346f
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-08-30 14:44:30 +0400

    created the design for the front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-
+import {Box, Button, Grid, TextField, Typography } from '@mui/material';
 import './App.css'
 import React, { useState } from "react";
 
@@ -10,9 +10,37 @@ const App = () => {
     setTextMutaion(value)
   }
 
+
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    console.log(textMutation);
+  }
+
+
   return (
     <>
-
+      <Grid
+        onSubmit={onSubmit}
+        container spacing={2} component="form" display="flex" alignItems="center" justifyContent="center" flexDirection="column">
+        <Grid item>
+          <Typography component="h4" variant="h4"> Shorten your link</Typography>
+        </Grid>
+        <Grid
+          item>
+          <TextField label="link" onChange={onChange} variant="filled" value={textMutation} >
+          </TextField>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary" type="submit">
+            Shornet
+          </Button>
+        </Grid>
+      </Grid>
+      <Box>
+        <Typography component="h4" variant="h4">
+          Here will be the link
+        </Typography>
+      </Box>
     </>
   )
 };
